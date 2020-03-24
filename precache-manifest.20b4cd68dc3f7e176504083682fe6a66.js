673c30278374d354f218e33132f128b4c9fc1183
ardato belay <aradatobelay@gmaill.com>
2020-03-24 19:39:52 +0200

    Updates

---
+++ precache-manifest.20b4cd68dc3f7e176504083682fe6a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1cb5f0da93f8544b4064f21a58b445",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "4d16d693547ac4fe085f",
+    "url": "/expense-tracker-react/static/css/main.891aca37.chunk.css"
+  },
+  {
+    "revision": "2b82d178e53d0d63af8e",
+    "url": "/expense-tracker-react/static/js/2.24e7ceeb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-tracker-react/static/js/2.24e7ceeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d16d693547ac4fe085f",
+    "url": "/expense-tracker-react/static/js/main.e4e56d54.chunk.js"
+  },
+  {
+    "revision": "e93a04e8233a83ef4413",
+    "url": "/expense-tracker-react/static/js/runtime-main.a0577cbc.js"
+  }
+]);
